8f1793762f996f8a3e66581880f3613f90ba89f7
unknown <abdoaswani1997@gmail.com>
2022-07-05 15:08:18 +0200

    add main components

--- src/app/modules/food-module/food-module.module.ts
+++ src/app/modules/food-module/food-module.module.ts
@@ -3,9 +3,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 //Components
 import { FoodModuleRoutingModule } from './food-module-routing.module';
+import { HomeComponent } from './home/home.component';
+import { FoodcardComponent } from './foodcard/foodcard.component';
+import { FoodrecipeComponent } from './foodrecipe/foodrecipe.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    FoodcardComponent,
+    FoodrecipeComponent
+  ],
   imports: [CommonModule, FoodModuleRoutingModule],
 })
 export class FoodModuleModule {}
